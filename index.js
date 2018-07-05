05b4170fb04290beb6a3d7a8599696c7277cfc20
Charith Wijenayake <CharithWijenayake@CLASS-SYSTEMS.local>
2018-07-05 08:58:26 +0100

    changes

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-export * from './app4';
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient; 
+
+var app = express();
+var dbString = 'mongodb://charithw:cbacharith2@ds125341.mlab.com:25341/quotes-db';
+var db;
+
+app.set('view engine', hbs);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(dbString, (error, client) => {
+    if(error){
+        console.log(error);
+    }
+    db = client.db('quotes-db');
+    app.listen('5000', () => {
+        console.log('server is up on port 5000');
+    });
+});
+
+app.get('/',(req, res)=>{
+    var cursor = db.collection('quotes').find().toArray((error, results)=>{
+        if(error){
+            console.log(error);
+        }
+        else{
+            console.log(results);
+            res.render('form.hbs',{results: results});
+        }
+        
+    });
+    
+});
+
+app.post('/quotes',(req, res) => {
+    db.collection('quotes').save(req.body, (error, result) => {
+        if(error){
+            console.log(error);
+        }
+        console.log('saved to database');
+        res.redirect('/');
+    });
+});
+
+
+
+
+
